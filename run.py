db1fae8147c1033843c1eb62cffc33f6d0ff76b3
Tank <63246618+coolrobby@users.noreply.github.com>
2025-05-07 09:32:03 +0800

    2

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ import altair as alt
 # 设置页面标题
 st.title("错题分析-英语")
 st.markdown(
-    "<p style='text-align: left; color: red;'>请上传xlsx文件进行分析。右上角有个小人在动，就表示正在运行。如果担心上课时候打不开，按键盘的“ctrl+p”,可以将当前页面保存为PDF。此外，点击右上角的三点图标，还可以进行一些设置，比如设置为宽屏。</p>",
+    "<p style='text-align: left; color: red;'>读取数据会很慢，请耐心等待。右上角有个小人在动，就表示正在运行。如果担心上课时候打不开，按键盘的“ctrl+p”,可以将当前页面保存为PDF。此外，点击右上角的三点图标，还可以进行一些设置，比如设置为宽屏。</p>",
     unsafe_allow_html=True)
 
-# 文件上传界面
-uploaded_file = st.file_uploader("请上传xlsx文件:", type=['xlsx'])
+# 上传Excel文件
+uploaded_file = st.file_uploader("请上传错题分析的Excel文件:", type=['xlsx'])
 
 if uploaded_file is not None:
     # 读取上传的文件
@@ -59,14 +59,25 @@ if uploaded_file is not None:
 
         i += 1  # 处理下一道题
 
+    # 添加排序选项
+    sort_option = st.selectbox("选择排序方式:", ["按照题目原本顺序", "按照正确率升序", "按照正确率降序"])
+
+    # 根据选择的排序方式进行排序
+    if sort_option == "按照正确率升序":
+        sorted_results = sorted(results, key=lambda x: x['正确率'])
+    elif sort_option == "按照正确率降序":
+        sorted_results = sorted(results, key=lambda x: x['正确率'], reverse=True)
+    else:
+        sorted_results = results  # 保持原本顺序
+
     # 创建导航栏
     st.sidebar.title("题目导航")
-    for res in results:
+    for res in sorted_results:
         question_link = f"[第{res['题号']}题 (正确率: {res['正确率']:.2f}%)](#{res['题号']})"
         st.sidebar.markdown(question_link)
 
-    # 显示题目统计
-    for res in results:
+    # 显示选择的题目统计
+    for res in sorted_results:
         st.markdown(f"<a id='{res['题号']}'></a>", unsafe_allow_html=True)  # 创建锚点
         st.subheader(f"第{res['题号']}题")
         st.write(f"题目: {res['试题']}")
@@ -92,13 +103,13 @@ if uploaded_file is not None:
             # 列出所有错误答案
             for _, row in error_stats.iterrows():
                 color = 'green' if row['答案'] == res['标准答案'] else 'red'
-                st.markdown(
-                    f"<div style='color:black;'>答案: <span style='color:{color};'>{row['答案']}</span></div>",
-                    unsafe_allow_html=True)
+                st.markdown(f"<div style='color:black;'>答案: <span style='color:{color};'>{row['答案']}</span></div>",
+                            unsafe_allow_html=True)
                 st.write(f"出现次数: {row['出现次数']}")
                 st.write(f"学生: {row['学生']}")
                 st.write("")  # 添加空行
 
     st.success("统计完成！")
+
 else:
-    st.info("请上传一个xlsx文件以开始分析。")
+    st.info("请上传一个Excel文件以进行错题分析。")
